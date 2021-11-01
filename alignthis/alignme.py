695684df0bf0a213a2845a1b55ddc9c63af2b613
Antonia Schumacher <antoniaschumacher@AS-MP.fritz.box>
2021-11-01 09:56:18 +0100

    add docstring

--- alignthis/alignme.py
+++ alignthis/alignme.py
@@ -1,11 +1,9 @@
-from Bio.Seq import Seq 
+from Bio.Seq import Seq
 
 
 def read_dna(file):
-	
-	#read sequence
-	with open(file, "r") as f:
-		seq = Seq(f.read().replace("\n", "").upper())
+    """Returns Sequence from a text file."""
+    with open(file, "r") as f:
+        seq = Seq(f.read().replace("\n", "").upper())
 
-	return seq
-	
+    return seq
--- useme.py
+++ useme.py
@@ -1,17 +1,16 @@
 from Bio import Align
+
 import alignthis.alignme as am
 
-s1 = am.read_dna('dnafile_1.txt')
-s2 = am.read_dna('dnafile_2.txt')
+s1 = am.read_dna("dnafile_1.txt")
+s2 = am.read_dna("dnafile_2.txt")
 
 aligner = Align.PairwiseAligner()
 aligner.match_score = 2
-aligner.mode = 'local'
+aligner.mode = "local"
 
 alignments = aligner.align(s1, s2)
 
-#print alignments
+# print alignments
 for a in alignments:
-	print(a)
-
-
+    print(a)
